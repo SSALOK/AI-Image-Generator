8ce81667e65d40f47d9a0abe3fc261d302feeba1
SSALOK <alokraj123421@gmail.com>
2023-03-11 15:49:29 +0530

    changed link of backend in home and createpost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,13 +16,16 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ prompt: form.prompt }),
-        });
+        const response = await fetch(
+          "https://dall-e-fwdz.onrender.com/api/v1/dalle",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: form.prompt }),
+          }
+        );
 
         const data = await response.json();
         const photoData = data.photo;
@@ -41,12 +44,15 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          // body: JSON.stringify(form),
-          body: JSON.stringify({ ...form }),
-        });
+        const response = await fetch(
+          "https://dall-e-fwdz.onrender.com/api/v1/post",
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            // body: JSON.stringify(form),
+            body: JSON.stringify({ ...form }),
+          }
+        );
         await response.json();
         console.log("home page", response);
         navigate("/");
